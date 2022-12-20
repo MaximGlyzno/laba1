417652df01eba5fffd89a877f2edc90c65606cce
Maxim <glyznomax@gmail.com>
2022-12-20 14:46:13 +0300

    first commit

---
+++ src/laba1/Food.java
@@ -0,0 +1,25 @@
+package laba1;
+public abstract class Food implements Consumble {
+    String name = null;int amount = 0;
+    public Food(String name) {
+        this.name = name;
+    }
+    public boolean equals(Object arg0) {
+        if (!(arg0 instanceof Food)) return false; // Шаг 1
+        if (name==null || ((Food)arg0).name==null) return false; // Шаг 2
+        return name.equals(((Food)arg0).name); // Шаг 3
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Реализация метода consume() удалена из базового класса Food
+// Это можно сделать, потому что сам Food - абстрактный
+
+}
